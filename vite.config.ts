7a29e1d1514eee59507c1c51f7d08cdba68c851d
ErgaAditya <168323995+ErgaAditya@users.noreply.github.com>
2025-11-27 19:19:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+// ergaaditya/endri-wibowo/endri-wibowo-a1e13af5ee491ee5defa75438c1afe9a91429ffe/vite.config.ts
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +7,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // TAMBAHKAN BASE URL INI
+      base: './', // Menggunakan path relatif untuk mengatasi masalah subpath di GH Pages
       server: {
         port: 3000,
         host: '0.0.0.0',
